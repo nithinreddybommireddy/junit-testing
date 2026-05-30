f6f88a40b89bd1fbd2a447e5b7b189b255ecea3c
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-05-30 16:25:00 +0530

    feat:Customer class using lombok

--- src/main/java/com/stschool/ecommerce/model/Customer.java
+++ src/main/java/com/stschool/ecommerce/model/Customer.java
@@ -1,10 +1,14 @@
 package com.stschool.ecommerce.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 public class Customer {
     private int id;
     private String name;
