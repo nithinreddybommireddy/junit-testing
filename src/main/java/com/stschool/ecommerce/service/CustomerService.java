906a591c2e18ba90d97ef0d02b2c6321d73d4bf3
nithinreddybommireddy <nithinreddybommireddy@gmail.com>
2026-05-30 16:25:42 +0530

    feat:Customer Service using streams

--- src/main/java/com/stschool/ecommerce/service/CustomerService.java
+++ src/main/java/com/stschool/ecommerce/service/CustomerService.java
@@ -1,6 +1,7 @@
 package com.stschool.ecommerce.service;
 
 import com.stschool.ecommerce.exception.CustomerExistsException;
+import com.stschool.ecommerce.exception.CustomerNotFoundException;
 import com.stschool.ecommerce.model.Customer;
 
 import java.util.List;
@@ -14,11 +15,32 @@ public class CustomerService {
 
     public Customer save(Customer customer) {
         customers.stream().filter(c -> c.getId() == customer.getId()).findFirst().ifPresent(c -> {
-            throw new CustomerExistsException("Customer already exists");
+            throw new CustomerExistsException("Customer already exists with ID: " + customer.getId());
         });
         customers.add(customer);
         return customer;
     }
 
+    public List<Customer> getAllCustomers() {
+        return customers;
+    }
 
+    public Customer getCustomerById(int id) {
+        return customers.stream().filter(c -> c.getId() == id).findFirst().orElseThrow(() -> new CustomerNotFoundException("Customer not found with ID: " + id));
+    }
+
+    public boolean deleteCustomer(int id) {
+        boolean removed = customers.removeIf(c -> c.getId() == id);
+        if (!removed) {
+            throw new CustomerNotFoundException("Customer not found with ID: " + id);
+        }
+        return true;
+
+    }
+
+    public double getTotalBalance() {
+        return customers.stream().mapToDouble(Customer::getBalance).sum();
+    }
 }
+
+
